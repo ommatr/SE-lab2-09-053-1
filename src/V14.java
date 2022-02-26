36ff0fc04c9be85f02191f9701b42106b38e90bc
Bulat <chumakov.bulat@mail.ru>
2022-02-26 14:48:29 +0300

    Булат создал свой вариант с методом

---
+++ src/V14.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class V14 {
+    public static void v14(double x){
+        double w=   Math.sqrt(Math.abs (Math.log (1-2*x*Math.cos((Math.PI/3)+x*x))));
+    }
+}
