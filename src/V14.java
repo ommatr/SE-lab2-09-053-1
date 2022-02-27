8016f169645f54c32670f9ac101e88f0a33b9bc7
Bulat <chumakov.bulat@mail.ru>
2022-02-27 14:08:16 +0300

    Изменил метод для своего варианта

--- src/V14.java
+++ src/V14.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class V14 {
     public static void v14(double x){
 
-        double w=   Math.sqrt(Math.abs (Math.log (1-2*x*Math.cos((Math.PI/3)+x*x))));
+        return Math.sqrt(Math.abs(Math.log(1-2*x*Math.cos(Math.PI/3)+x*x)));
     }
 }
