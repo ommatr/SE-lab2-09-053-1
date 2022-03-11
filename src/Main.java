e90e3f0d8e3d26334aac31f860d791c01cbbf314
Gulnaz <MILANURRUS@yandex.ru>
2022-03-11 20:17:03 +0300

    Add Kamilla's func

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ public class Main {
                 case "v0" -> System.out.println(V0.func(Double.parseDouble(args[1])));
                 case "v2"-> System.out.println(v2.func(Double.parseDouble(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3]), Double.parseDouble(args[4])));
                 case "v16"-> System.out.println(V16.func(Double.parseDouble(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3]), Double.parseDouble(args[4]), Double.parseDouble(args[5])));
-                case "v2"-> System.out.println(v2.func(Double.parseDouble(args[1])));
+                //case "v2"-> System.out.println(v2.func(Double.parseDouble(args[1]))); что-то неправильно тут
                 case "v14"->  System.out.println(V14.v14(Double.parseDouble(args[1])));
 		        case "v7" -> System.out.println(V7.func(Double.parseDouble(args[1])));
                 case "v13"->System.out.println(V13.func(Double.parseDouble(args[1])));
+                case "v6" -> System.out.println(V6.function(Double.parseDouble(args[1])));
                 case "v9"-> System.out.println(V9.funcV9(Double.parseDouble(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3])) );
                 case "exit" -> {
                     System.out.println("Done");
---
+++ src/V6.java
@@ -0,0 +1,5 @@
+public class V6 {
+    public static double function(double x) {
+        return Math.pow(Math.E, (2 * Math.sin(4 * x) + Math.cos(x * x) * Math.cos(x * x)) / 3 * x);
+    }
+}
